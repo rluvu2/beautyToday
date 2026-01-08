9a08541a9601160696312adc99c0d8393d0aa922
DESKTOP-52BANKH\rluvu <rluvu2@nate.com>
2026-01-08 17:47:20 +0900

    video 페이지 추가

--- assets/main.js
+++ assets/main.js
@@ -11,9 +11,9 @@ window.addEventListener('DOMContentLoaded', function() {
   if (fetchBtn) fetchBtn.onclick = function() {
     // 데모용 더미 데이터
       const videos = [
-        { id: 1, title: '화장품 소개1', thumb: 'https://img.youtube.com/vi/WkGG0HZdGyc/0.jpg', url: 'video1.html' },
-        { id: 2, title: '피부관리 노하우', thumb: 'http://phaeton.firestick.live/media/8296/highlighting/cut0_thumb.jpg', url: 'video2.html' },
-        { id: 3, title: '여러가지 팩 장단점', thumb: 'http://phaeton.firestick.live/media/4408/highlighting/cut0_thumb.jpg', url: 'video3.html' }
+        { id: 1, title: '화장품 소개1', thumb: 'https://img.youtube.com/vi/WkGG0HZdGyc/0.jpg', url: 'https://rluvu2.github.io/beautyToday/video1.html' },
+        { id: 2, title: '피부관리 노하우', thumb: 'https://img.youtube.com/vi/fYk0CKfPXhk/0.jpg', url: 'https://rluvu2.github.io/beautyToday/video2.html' },
+        { id: 3, title: '여러가지 팩 장단점', thumb: 'https://img.youtube.com/vi/6soPMWrJiNc/0.jpg', url: 'https://rluvu2.github.io/beautyToday/video3.html' }
       ];
     const list = document.getElementById('videos-list');
     list.innerHTML = '';
